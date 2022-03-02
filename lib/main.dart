df70a7ce2a23576f60c5cac8193dd29e738280c8
Vinicius <vinnievandro@hotmail.com>
2022-03-02 16:49:30 -0300

    +1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 //! inicio de um sonho
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
